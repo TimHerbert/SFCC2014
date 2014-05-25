de0482fb10e8ef580c2c0065ba5a34aba42ed892
Gary Bailey <garybailey@Garys-iMac.local>
2014-05-24 23:07:09 -0700

    no message

---
+++ school/js/index.js
@@ -0,0 +1,492 @@
+var json = [
+{
+  'id' : '0', 
+  'name' : 'Annie Lindley',
+  'category' : 'interactive-designer',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://www.sfccdesign.com/img/annie/bright_futures.jpg',
+  'img2' : 'http://www.sfccdesign.com/img/annie/FriendFinderApp_web.jpg',
+  'img3' : 'http://www.sfccdesign.com/img/annie/jabberwock_web.jpg',
+  'img4' : 'http://www.sfccdesign.com/img/annie/SpIFFlandingpage_web.jpg',
+},
+{
+  'id' : '1', 
+  'name' : 'David Turovskiy',
+  'category' : 'developer',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '2', 
+  'name' : 'Dayna Mowery',
+  'category' : 'illustrator',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+    'id' : '3', 
+    'name' : 'Gary Bailey',
+    'category' : 'interactive-design',
+    'email' : 'timothyherbert@hotmail.com',
+    'profile' : 'http://lorempixel.com/g/480/480',
+    'link' : 'http://garybaileydesign.com/',
+    'img' : 'http://lorempixel.com/g/480/480',
+    'img1' : 'http://lorempixel.com/g/480/480',
+    'img2' : 'http://lorempixel.com/g/480/480',
+    'img3' : 'http://lorempixel.com/g/480/480',
+    'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '4', 
+  'name' : 'Jesse Thomas',
+  'category' : 'developer',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.jesset.net',
+  'img' : 'http://jesset.net/hosted/2.jpg',
+  'img1' : 'http://jesset.net/hosted/1.jpg',
+  'img2' : 'http://jesset.net/hosted/3.jpg',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '5', 
+  'name' : 'Jonna Kelley',
+  'category' : 'interactive-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},  
+{
+  'id' : '6', 
+  'name' : 'Laura Obrien',
+  'category' : 'illustrator',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '7', 
+  'name' : 'Lilia Zgherea',
+  'category' : 'print-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '8', 
+  'name' : 'Liorah Wichser',
+  'category' : 'print-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://www.sfccdesign.com/img/liorah/cerealbox-snip.jpg',
+  'img1' : 'http://www.sfccdesign.com/img/liorah/cow-snip.jpg',
+  'img2' : 'http://www.sfccdesign.com/img/liorah/flying-snip.png',
+  'img3' : 'http://www.sfccdesign.com/img/liorah/kind-leaves-snip.png',
+  'img4' : 'http://www.sfccdesign.com/img/liorah/project-hope-logo-bw.jpg',
+}, 
+{
+  'id' : '9', 
+  'name' : 'Melanie Hoff',
+  'category' : 'interactive-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '10', 
+  'name' : 'Michael Stanich',
+  'category' : 'developer',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://michaelstanich.net/',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},  
+{
+  'id' : '11', 
+  'name' : 'Mickenzie Burns',
+  'category' : 'multimedia',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '12', 
+  'name' : 'Patrick Marko',
+  'category' : 'interactive-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},  
+{
+  'id' : '13', 
+  'name' : 'Preston Curtis',
+  'category' : 'Web Developer',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},  
+{
+  'id' : '14', 
+  'name' : 'Reba Kluesner',
+  'category' : 'print-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '15', 
+  'name' : 'Robert Andrews',
+  'category' : 'multimedia',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '16', 
+  'name' : 'Ryan Flannery',
+  'category' : 'Web Developer',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},  
+{
+  'id' : '17', 
+  'name' : 'Serafina Bui',
+  'category' : 'print-design',
+  'email' : 'timothyherbert@hotmail.com',  
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},  
+{
+  'id' : '18', 
+  'name' : 'Shayla McGhee',
+  'category' : 'interactive-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '19', 
+  'name' : 'Simon Hartt',
+  'category' : 'print-design',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '20', 
+  'name' : 'Thair Gorgees',
+  'category' : 'Web Developer',
+  'email' : 'timothyherbert@hotmail.com',
+  'profile' : 'http://lorempixel.com/g/480/480',
+  'link' : 'http://www.sfccdesign.com',
+  'img' : 'http://lorempixel.com/g/480/480',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',
+},
+{
+  'id' : '21', 
+  'name' : 'Tim Herbert',
+  'category' : 'developer',
+  'email' : 'timothyherbert@hotmail.com',
+  'title' : 'http://lorempixel.com/g/480/480',
+  'profile' : 'http://timherbert.net/hosted/profile.jpg',
+  'link' : "http://timherbert.net/",
+  'img' : 'http://timherbert.net/hosted/1.png',
+  'img1' : 'http://lorempixel.com/g/480/480',
+  'img2' : 'http://lorempixel.com/g/480/480',
+  'img3' : 'http://lorempixel.com/g/480/480',
+  'img4' : 'http://lorempixel.com/g/480/480',  
+},
+            
+]; // End json object
+/*
+Attempt at global from local function
+*/
+          
+/********************************************
+              Gallery Functions
+*********************************************/            
+var click_toggle = 0;
+var data_click = 0;
+$(window).load(function(){ 
+
+var copy = json.slice();
+
+while( copy.length ) {
+    var keyword = copy.splice( Math.floor(Math.random()*copy.length), 1 );
+    //document.write(keyword[0].id + '<br>');
+    output = '<div data-id="'+keyword[0].id+'" class="cardWrapper '+keyword[0].category+'" data-category="">';
+    output += '<img src="'+keyword[0].img+'" class="third '+'" alt="image">'; 
+    output += '<img src="http://garybaileydesign.com/test_2/second.png" class="second '+'" alt="image">'; 
+    output += '<img src="http://garybaileydesign.com/test_2/first.png" class="first '+'" alt="image">'; 
+    output += '</div>';
+    output += '<div id="'+ keyword[0].id +'" class="info-mod info-mod-' + keyword[0].id + '">';
+    output += '</div>';
+$('#gallery').append(output);
+
+           
+}
+
+//
+$(document).click(function(){
+  $('.info-mod').slideUp(200);
+  click_toggle = 0;
+   $('.cardWrapper, .container').css('opacity', '1'); TweenLite.to($('.cardWrapper').find(".card"), 1.2, {rotationY:0});
+  $('body').css('cursor', 'default');
+  $('.cardWrapper, .front, .back').css('cursor', 'pointer');
+  $('.info-mod').empty();
+  data_click = 0;
+});
+
+$('.cardWrapper').click(function(e){  
+  if (click_toggle === 0 ){    
+      //console.log(click_toggle);
+    e.stopPropagation();
+    click_toggle = 1;
+    data_click = 1;
+    var index = $(this).attr('data-id');
+  
+    output = '<div class="fluid-container">';
+    
+    output += '<div class="info-mod-col info-contact">';
+    output += '<h4>'+json[index].name+'</h4>';
+    output += '<p><a href="'+json[index].link+'" target="_blank">'+json[index].link+'</a></p>';
+    output += '<p><a href="mailto:"'+json[index].email+'">'+json[index].email+'</a></p>';
+    output += '</div>'; // end col-sm-3
+    
+    output += '<div class="info-mod-col">';  
+    output += '<img class="profile"  src="'+json[index].img1+'" alt="SFCC Grad Show 2014">';
+    output += '</div>';
+
+    output += '<div class="info-mod-col">';
+    output += '<img class="profile1"  src="'+json[index].img2+'" alt="SFCC Grad Show 2014">';
+    output += '</div>'; // end col-sm-3
+ 
+    output += '<div class="info-mod-col">';
+    output += '<img class="profile1"  src="'+json[index].img3+'" alt="SFCC Grad Show 2014">';
+    output += '</div>'; // end col-sm-3
+   
+    output += '</div>'; // end fluid-container
+    
+    $(output).appendTo('.info-mod-'+index);
+    $(".info-mod-"+index).slideDown(300);
+    TweenLite.to($(this).find(".card"), 0.7, {rotationY:180});
+
+    $('body, .cardWrapper, .front, .back').css('cursor', 'url(img/x-cursor.png), auto');
+
+    $('.cardWrapper, .container').not(this).css('opacity', '0.1');
+
+    $(window).scrollTop($(this).offset().top );
+  }
+
+
+    });
+  
+
+
+var timeoutId;
+
+if(data_click == '0') {
+$(".cardWrapper").hover(function() {
+  var index = $(this);
+  //console.log(data_click+' data click');
+  if (!timeoutId && data_click == 0) {
+            timeoutId = window.setTimeout(function() {
+                timeoutId = null;
+                TweenLite.to($(index).find(".card"), 1, {rotationY:180});
+              //alert('hi');
+           }, 400);
+        }
+    },
+    function () {
+        if (timeoutId) {
+            window.clearTimeout(timeoutId);
+            timeoutId = null;
+        }
+        else if (data_click == 0) {
+           TweenLite.to($(this).find(".card"), 0.7, {rotationY:0}); 
+        } else {
+          
+        }
+    });
+}
+
+TweenLite.set(".cardWrapper", {perspective:800});
+TweenLite.set(".card", {transformStyle:"preserve-3d"});
+TweenLite.set(".back", {rotationY:-180});
+TweenLite.set([".back", ".front"], {backfaceVisibility:"hidden"});
+
+
+
+
+//Remote not working yet
+
+$('.remote a').click(function(e){  
+  if (click_toggle === 0 ){    
+      //console.log(click_toggle);
+    e.stopPropagation();
+    click_toggle = 1;
+    data_click = 1;
+    var index = $(this).attr('data-id');
+    //var filter_var = $().attr('class')
+    var filter = $('cardWrapper').filter().attr('data-id', index);
+    output = '<div class="fluid-container">';
+    
+    output += '<div class="col-sm-3">';
+    output += '<p>'+json[index].name+'</p>';
+    output += '<p>'+json[index].link+'</p>';
+    output += '<p>'+json[index].email+'</p>';
+    output += '</div>'; // end col-sm-3
+    
+    output += '<div class="col-sm-3">';  
+    output += '<img class="profile"  src="'+json[index].img1+'" alt="SFCC Grad Show 2014">';
+    output += '</div>';
+
+    output += '<div class="col-sm-3">';
+    output += '<img class="profile1"  src="'+json[index].img2+'" alt="SFCC Grad Show 2014">';
+    output += '</div>'; // end col-sm-3
+
+    output += '<div class="col-sm-3">';
+    output += '<img class="profile1"  src="'+json[index].img3+'" alt="SFCC Grad Show 2014">';
+    output += '</div>'; // end col-sm-3
+   
+    output += '</div>'; // end fluid-container
+    
+    var filter = $('.cardWrapper').filter(function(){
+      return $(this).attr('data-id').match(index)
+    });
+    //var remote_trigger = $('.cardWrapper').find('data-id', index);
+    $(output).appendTo('.info-mod-'+index); 
+    $(".info-mod-"+index).slideDown(300);
+    //TweenLite.to($(filter).find(".card"), 1.2, {rotationY:180});
+    $('body, .cardWrapper').css('cursor', 'url(img/x-cursor.png), auto');
+     
+    setTimeout(function(){
+       $(window).scrollTop($(filter).offset().top);
+    }, 10);
+    
+    
+    //console.log(filter);
+    //console.log(remote_trigger);
+    $('.cardWrapper').not(filter).css('opacity', '0.4');
+    
+    
+  }
+});
+    
+  $('.portfolioFilter a').click(function(){
+    $('.cardWrapper').css('display', 'inline-block');     
+    $('.portfolioFilter .current').removeClass('current');
+    $(this).addClass('current');
+    var selector = $(this).attr('data-filter');
+    $('.cardWrapper').not(selector).css('display', 'none');    
+    });
+
+}); // end of load function
+
+//Not working yet
+// Remote Loop
+for(var i = 0; i < json.length; i++) {
+    output1 = '<li class="remote">';
+    //output1 += '<a href="'json[i].id'">';
+    output1 += '<a href="#" data-id="'+json[i].id+'">';
+    output1 += json[i].name;
+    output1 += '</a>';
+    output1 += '</li>';
+
+    $('#students').append(output1);
+}
+
+
+
+
+ 
